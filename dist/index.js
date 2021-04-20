ee285dcee4b15b165059e224b8d189b8ce78bcde
Maxime Evangeliste <max.evangeliste@gmail.com>
2021-04-19 23:58:27 -0400

    Add strict mode to tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@([a-zA-Z0-9-]+[\.][a-zA-Z0-9-]{2,})+$/;
-export default (function (str) {
+exports.default = (function (str) {
     return regex.test(String(str).toLowerCase());
 });
