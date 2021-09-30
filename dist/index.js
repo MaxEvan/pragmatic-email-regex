2ed7c1378cf0765cfe88c5df16d1c6d26ec5cf36
Maxime Evangeliste <max.evangeliste@gmail.com>
2021-09-30 10:26:06 -0400

    Include built dist code 🤦 and bump version again...

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@([a-zA-Z0-9-]+[\.][a-zA-Z0-9-]{2,})+$/;
+var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 exports.default = (function (str) {
     return regex.test(String(str).toLowerCase());
 });
